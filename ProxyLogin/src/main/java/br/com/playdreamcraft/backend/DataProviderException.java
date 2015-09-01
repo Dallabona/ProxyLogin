f7b77bfea222312f771677f7549ecfce8e3f6a49
PalitoDeDente <lucas-d@outlook.com>
2015-09-01 01:18:03 -0300

    DataProvider exception

---
+++ ProxyLogin/src/main/java/br/com/playdreamcraft/backend/DataProviderException.java
@@ -0,0 +1,14 @@
+package br.com.playdreamcraft.backend;
+
+public class DataProviderException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= -4515738808792207884L;
+	public DataProviderException(String message)
+	{
+		super(message);
+	}
+}
