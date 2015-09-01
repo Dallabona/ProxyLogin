bd2ef1bbe4cab5c22ba5f37ca75d971dfaddad27
PalitoDeDente <lucas-d@outlook.com>
2015-09-01 01:20:02 -0300

    DataProviderException

--- ProxyLogin/src/main/java/br/com/playdreamcraft/dao/ContaDAO.java
+++ ProxyLogin/src/main/java/br/com/playdreamcraft/dao/ContaDAO.java
@@ -3,6 +3,7 @@ package br.com.playdreamcraft.dao;
 import javax.security.auth.login.AccountNotFoundException;
 
 import br.com.playdreamcraft.account.Conta;
+import br.com.playdreamcraft.backend.DataProviderException;
 
 public interface ContaDAO
 {
@@ -10,7 +11,7 @@ public interface ContaDAO
 	 * Persiste uma conta
 	 * @param conta Conta a ser persistida
 	 */
-	public void inserirConta(Conta conta);
+	public void inserirConta(Conta conta) throws DataProviderException;
 	
 	/**
 	 * Recupera uma conta já persistida pelo nome
@@ -18,7 +19,7 @@ public interface ContaDAO
 	 * @return Retorna a conta
 	 * @throws AccountNotFoundException A conta pode nao existir
 	 */
-	public Conta getContaPorNome(String nome) throws AccountNotFoundException;	
+	public Conta getContaPorNome(String nome) throws AccountNotFoundException,DataProviderException;	
 	
 	
 	/**
@@ -26,7 +27,7 @@ public interface ContaDAO
 	 * @param conta Conta a ser deletada
 	 * @throws AccountNotFoundException A conta pode nao existir
 	 */
-	public void deletarConta(Conta conta)throws AccountNotFoundException;	
+	public void deletarConta(Conta conta)throws AccountNotFoundException,DataProviderException;	
 	
 	
 	/**
@@ -34,5 +35,5 @@ public interface ContaDAO
 	 * @param conta Conta em questão
 	 * @throws AccountNotFoundException A conta pode não existir
 	 */
-	public void atualizarConta(Conta conta) throws AccountNotFoundException;
+	public void atualizarConta(Conta conta) throws AccountNotFoundException,DataProviderException;
 }
