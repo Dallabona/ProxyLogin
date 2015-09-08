133b190b098fd56dc2af573641dd008f9de2d2bb
PalitoDeDente <lucas-d@outlook.com>
2015-09-07 22:24:59 -0300

    Hash test

--- ProxyLogin/src/test/java/br/com/playdreacraft/proxylogin/tests/HashSHA256Test.java
+++ ProxyLogin/src/test/java/br/com/playdreacraft/proxylogin/tests/HashSHA256Test.java
@@ -12,7 +12,7 @@ public class HashSHA256Test
 		HashSHA256 hash = new HashSHA256();
 		
 		// System.out.println(hash.gerarHash("aa", "123"));
-System.out.println(hash.senhaCorreta("$SHA$123$23596a0cc989b4625c10782450a511eafdd026a14a67a942667d8c4a83c80c32"
+System.out.println(hash.senhaCorreta("as"
 , "aa"));
 	}
 
